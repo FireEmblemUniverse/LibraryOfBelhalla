ef24667e60411a90f910423ecb5f6365b57eb453
Cameron Wong <cjwong@andrew.cmu.edu (lancer)>
2017-08-23 22:03:01 -0400

    add some services

---
+++ management/services/run_belhalla.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import sys, os
+import atexit
+import tempfile as tf
+
+def do_pull(user):
+    cwd = os.getcwd()
+    os.chdir(os.path.join(cwd, user))
+    os.system('git pull')
+    os.chdir(cwd)
+
+def do_create(user, rpath):
+    os.system(f'git clone {rpath} {user}')
+
+def process(line):
+    if not line: return
+    cmd, *args = line.split()
+    try:
+        if cmd == 'refresh': do_pull(*args)
+        elif cmd == 'init': do_create(*args)
+        # Do nothing
+        else: return
+    except TypeError:
+        return
+
+def belhalla(pipe):
+    global FIFO
+    FIFO = pipe
+    os.mkfifo(pipe)
+
+    for line in notify_loop():
+        process(line)
+
+def notify_loop(fname=None):
+    if fname is None: fname = FIFO
+    # This is necessary due to a quirk with Unix FIFO pipes; writing to the
+    # pipe will send an EOF (no more input) after each write. Thus, to keep the
+    # pipe open while blocking, we have to reopen it every time.
+    while True:
+        with open(fname) as f:
+            yield f.read()
+
+@atexit.register
+def clean():
+    try:
+        os.unlink(FIFO)
+    except:
+        # Can't do anything about this
+        pass
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2: belhalla(os.environ['BELHALLA_PIPE'])
+    else: belhalla(sys.argv[1])
+
